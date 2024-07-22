7473243754217a6973e0fb170bad676504a3fada
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-07-22 14:34:02 -0600

    feat: add Codigo section

--- slices/Codigo/index.vue
+++ slices/Codigo/index.vue
@@ -18,26 +18,127 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    <PrismicRichText :field="slice.primary.title" />
-    {{ slice.primary.subtitle }}
-    <template v-for="item in slice.primary.groupellos">
-      {{ item.subtitle }}
-      {{ item.label1 }}
-      {{ item.label2 }}
-      {{ item.label3 }}
-      {{ item.label4 }}
-      {{ item.label5 }}
-      {{ item.label6 }}
-    </template>
-    <PrismicImage :field="slice.primary.image" />
-    <template v-for="item in slice.primary.groupellas">
-      {{ item.subtitle }}
-      {{ item.label1 }}
-      {{ item.label2 }}
-      {{ item.label3 }}
-      {{ item.label4 }}
-      {{ item.label5 }}
-      {{ item.label6 }}
-    </template>
+    <div class="codigo-slice">
+      <div class="codigo-slice__container">
+        <div class="codigo-slice__title">
+          <h2><PrismicRichText :field="slice.primary.title" /></h2>
+        </div>
+        <div class="codigo-slice__subtitle">{{ slice.primary.subtitle }}</div>
+        <div class="codigo-slice__columns">
+          <div class="codigo-slice__column1">
+            <template v-for="item in slice.primary.groupellos">
+              <div class="codigo-slice__text1">{{ item.subtitle }}</div>
+              <div class="codigo-slice__text">{{ item.label1 }}</div>
+              <div class="codigo-slice__text">{{ item.label2 }}</div>
+              <div class="codigo-slice__text">{{ item.label3 }}</div>
+              <div class="codigo-slice__text">{{ item.label4 }}</div>
+              <div class="codigo-slice__text">{{ item.label5 }}</div>
+              <div class="codigo-slice__text">{{ item.label6 }}</div>
+            </template>
+          </div>
+          <div class="codigo-slice__column2">
+            <PrismicImage :field="slice.primary.image" />
+          </div>
+          <div class="codigo-slice__column3">
+            <template v-for="item in slice.primary.groupellas">
+              <div class="codigo-slice__text1">{{ item.subtitle }}</div>
+              <div class="codigo-slice__text">{{ item.label1 }}</div>
+              <div class="codigo-slice__text">{{ item.label2 }}</div>
+              <div class="codigo-slice__text">{{ item.label3 }}</div>
+              <div class="codigo-slice__text">{{ item.label4 }}</div>
+              <div class="codigo-slice__text">{{ item.label5 }}</div>
+              <div class="codigo-slice__text">{{ item.label6 }}</div>
+            </template>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
+
+
+<style>
+  .codigo-slice {
+    width: 100%;
+    padding: 100px 0;
+  }
+  .codigo-slice__container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .codigo-slice__title { 
+    font-family: 'Gistesy', sans-serif;
+    font-size: 2.5rem;
+    padding-bottom: 50px;
+  }
+
+  .codigo-slice__subtitle {
+    font-family: 'Belleza', sans-serif;
+    font-size: 1.5rem;
+  }
+
+  .codigo-slice__columns {
+    margin: 30px 0;
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .codigo-slice__column1 {
+    width: 25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .codigo-slice__column2 {
+    width: 60%;
+    height: 200px;
+    background-color: rgb(176, 176, 176);
+    margin-bottom: 30px;
+  }
+  .codigo-slice__column2 img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .codigo-slice__column3 {
+    width: 25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .codigo-slice__text1 {
+    font-family: 'Gistesy', sans-serif;
+    font-size: 2rem;
+  }
+  .codigo-slice__text {
+    font-family: 'Belleza', sans-serif;
+    padding: 5px 0;
+    font-size: 1rem;
+  }
+
+  @media (min-width: 900px) {
+    .codigo-slice__columns {
+      margin: 30px 0;
+      width: 80%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    .codigo-slice__column2 {
+      width: 40%;
+      height: 400px;
+      background-color: rgb(176, 176, 176);
+    }
+  }
+
+</style>
